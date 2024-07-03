01aaa7d30e799e39b6e6b3f7ea44a8e3aedb94b3
jmbae <jmbae@codegrove.co.kr>
2024-07-03 13:46:02 +0900

    Initialize MotionApp

---
+++ MotionApp/MotionApp/MotionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionAppApp.swift
+//  MotionApp
+//
+//  Created by Jungman Bae on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MotionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
