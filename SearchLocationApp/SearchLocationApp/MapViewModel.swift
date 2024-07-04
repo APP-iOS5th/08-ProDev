7f695cffdf36a63865c4f8daea500a6b78b03b01
jmbae <jmbae@codegrove.co.kr>
2024-07-04 16:30:18 +0900

    CoreLocation 위치정보 업데이트

--- SearchLocationApp/SearchLocationApp/MapViewModel.swift
+++ SearchLocationApp/SearchLocationApp/MapViewModel.swift
@@ -9,8 +9,29 @@ import SwiftUI
 import MapKit
 
 @Observable
-class MapViewModel {
+class MapViewModel: NSObject, CLLocationManagerDelegate {
     var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
     var searchText = ""
     var mapStyle: MapStyle = .standard
+    
+    private var locationManager: CLLocationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func moveToCurrentLocation() {
+        cameraPosition = .userLocation(fallback: .automatic)
+    }
+    
+    // MARK: CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("error \(error.localizedDescription)")
+    }
 }
