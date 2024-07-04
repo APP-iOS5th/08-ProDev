33ccd5382b63598f0580384b54e93f8c7a3badb6
jmbae <jmbae@codegrove.co.kr>
2024-07-04 16:14:57 +0900

    MapView 생성

---
+++ SearchLocationApp/SearchLocationApp/MapViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewModel.swift
+//  SearchLocationApp
+//
+//  Created by Jungman Bae on 7/4/24.
+//
+
+import SwiftUI
+import MapKit
+
+@Observable
+class MapViewModel {
+    var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    var searchText = ""
+    var mapStyle: MapStyle = .standard
+}
