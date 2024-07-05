2339517869186106531a0c33f343b39cb30889f4
jmbae <jmbae@codegrove.co.kr>
2024-07-05 16:09:27 +0900

    MediaPlayerViewController 추가

--- MediaPlayerApp/MediaPlayerApp/MediaListTableViewController.swift
+++ MediaPlayerApp/MediaPlayerApp/MediaListTableViewController.swift
@@ -10,40 +10,41 @@ import UIKit
 class MediaListTableViewController: UITableViewController {
     
     let items: [MediaItem] = MediaItem.samples
-
+    var selectedIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "MediaPlayer"
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mediaItemCell")
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return items.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "mediaItemCell", for: indexPath)
         
         let item = items[indexPath.row]
-
-        var config = cell.defaultContentConfiguration()    
+        
+        var config = cell.defaultContentConfiguration()
         config.text = item.title
         config.image = UIImage(systemName: item.isVideo ? "video.square.fill" : "music.note")
         cell.accessoryType = .disclosureIndicator
@@ -53,13 +54,29 @@ class MediaListTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = items[indexPath.row]
-        
-        let viewController = ViewController()
+        configurePlayerViewController(indexPath.row)
+    }
+    
+    func configurePlayerViewController(_ itemIndex: Int) {
+        guard items.indices.contains(itemIndex) else { return }
+        let item = items[itemIndex]
+        let viewController = MediaPlayerViewController(item: item)
+        if itemIndex > 0 {
+            viewController.goPrev = { [weak self] in
+                self?.navigationController?.popViewController(animated: true)
+                self?.configurePlayerViewController(itemIndex - 1)
+            }
+        }
+        if itemIndex < items.count - 1 {
+            viewController.goNext = { [weak self] in
+                self?.navigationController?.popViewController(animated: true)
+                self?.configurePlayerViewController(itemIndex + 1)
+            }
+        }
         navigationController?.pushViewController(viewController, animated: true)
     }
-
 }
+
 
 #Preview {
     UINavigationController(rootViewController: MediaListTableViewController())
