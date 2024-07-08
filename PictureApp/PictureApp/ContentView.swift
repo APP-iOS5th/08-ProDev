d060e72e0ed5aba93f2680b14e279f12b7106591
jmbae <jmbae@codegrove.co.kr>
2024-07-08 16:29:15 +0900

    카메라 기능 오류 (수정안됨)

--- PictureApp/PictureApp/ContentView.swift
+++ PictureApp/PictureApp/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State private var showSheet = false
     @State private var image = UIImage()
-    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var sourceType: UIImagePickerController.SourceType?
     
     var body: some View {
         VStack {
@@ -30,8 +30,10 @@ struct ContentView: View {
                 }
                 Spacer()
                 Button(action: {
-                    sourceType = .camera
-                    showSheet = true
+                    DispatchQueue.main.async {
+                        sourceType = .camera
+                        showSheet = true
+                    }
                 }) {
                     Text("Camera")
                         .font(.title2)
@@ -41,8 +43,12 @@ struct ContentView: View {
         .padding()
         .sheet(isPresented: $showSheet) {
             // 첫번째 카메라 선택시 사진 라이브러리가 뜨는 문제가 있음.
-            ImagePicker(sourceType: sourceType, chosenImage: $image)
+            // Photo Library 를 한 번 눌러야 Camera 버튼이 제대로 동작함
+            if let sourceType = sourceType {
+                ImagePicker(sourceType: sourceType,
+                            chosenImage: $image)
                 .ignoresSafeArea()
+            }
         }
     }
 }
