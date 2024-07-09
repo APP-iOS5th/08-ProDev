8e3ce264b5509fd8326be75feb877b7d23d041c7
jmbae <jmbae@codegrove.co.kr>
2024-07-09 09:17:11 +0900

    FacialRecognitionApp

---
+++ FacialRecognitionApp/FacialRecognitionApp/FacialRecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialRecognitionAppApp.swift
+//  FacialRecognitionApp
+//
+//  Created by Jungman Bae on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
